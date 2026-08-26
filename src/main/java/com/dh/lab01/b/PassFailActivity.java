cbe9cc17295b39617ff5c7db3915a367367676cf
daniel <daniel.haddadeen1@gmail.com>
2026-08-26 19:07:11 -0400

    Added PassFailActivity Class

---
+++ src/main/java/com/dh/lab01/b/PassFailActivity.java
@@ -0,0 +1,25 @@
+package com.dh.lab01.b;
+
+/**
+ * @author 2588731
+ */
+public class PassFailActivity extends GradedActivity {
+    private double minPassingScore;
+    
+    public PassFailActivity(double mps) {
+        minPassingScore = mps;
+    }
+    
+    /**
+     * Returns a passing letter grade based on whether the score is higher or
+     * lower than the minimum passing score.
+     * @return 'P' if the score is higher or equal to the minimum passing score. 'F' if it is less.
+     */
+    public char getGrade() {
+        if (getScore() >= minPassingScore) {
+            return 'P';
+        } else {
+            return 'F';
+        }
+    }
+}
