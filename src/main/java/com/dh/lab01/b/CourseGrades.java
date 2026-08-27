7661a6637f1e61828385764a4c8d93b1aaf0f183
daniel <daniel.haddadeen1@gmail.com>
2026-08-27 12:31:52 -0400

    Added CourseGrades Class

---
+++ src/main/java/com/dh/lab01/b/CourseGrades.java
@@ -0,0 +1,91 @@
+package com.dh.lab01.b;
+
+/**
+ * @author Daniel Haddadeen (2588731)
+ */
+public class CourseGrades implements Analyzable {
+    private GradedActivity[] grades;
+    private static int NUM_GRADES = 4;
+    
+    public CourseGrades() {
+        grades = new GradedActivity[NUM_GRADES];
+    }
+    
+    public void setLab(GradedActivity aLab) {
+        grades[1] = aLab;
+    }
+    
+    public void setPassFailExam(PassFailExam aPassFailExam) {
+        grades[2] = aPassFailExam;
+    }
+    
+    public void setEssay(Essay anEssay) {
+        grades[3] = anEssay;
+    }
+    
+    public void setFinalExam(FinalExam aFinalExam) {
+        grades[4] = aFinalExam;
+    }
+
+    @Override
+    public String toString() {
+        String str = String.format(
+                "%s: %.1f %-30s: %c\n" 
+                + "%s Score: %.1f %-30s: %c\n" 
+                + "%s: %.1f %-30s: %c\n"
+                + "%s: %.1f %-30s: %c\n", 
+                "Lab Score", grades[1].getScore(), "Grade", grades[1].getGrade(),
+                "Pass/Fail Exam Score", grades[2].getScore(), "Grade", grades[2].getGrade(),
+                "Essay Score", grades[3].getScore(), "Grade", grades[3].getGrade(),
+                "Final Exam Score", grades[4].getScore(), "Grade", grades[4].getGrade()
+        );
+        
+        return str;
+    }
+    
+    /**
+     * Returns the average score of all the graded activities.
+     * @return The average score of all the graded activities.
+     */
+    public double getAverage() {
+        double total = 0;
+        
+        for (GradedActivity grade : grades) {
+            total += grade.getScore();
+        }
+        
+        return total / NUM_GRADES;
+    }
+    
+    /**
+     * Returns the graded activity with the highest score.
+     * @return The graded activity with the highest score
+     */
+    public GradedActivity getHighest() {
+        GradedActivity highest = grades[1];
+        
+        for (GradedActivity grade : grades) {
+            if (grade.getScore() > highest.getScore()) {
+                highest = grade;
+            }
+        }
+        
+        return highest;
+    }
+    
+    /**
+     * Returns the graded activity with the lowest score.
+     * @return The graded activity with the lowest score
+     */
+    public GradedActivity getLowest() {
+        GradedActivity lowest = grades[1];
+        
+        for (GradedActivity grade : grades) {
+            if (grade.getScore() < lowest.getScore()) {
+                lowest = grade;
+            }
+        }
+        
+        return lowest;
+    }
+}
