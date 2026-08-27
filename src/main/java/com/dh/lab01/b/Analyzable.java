89f43500b440a1fc5fd14b54d806e51407c30f61
daniel <daniel.haddadeen1@gmail.com>
2026-08-27 12:54:38 -0400

    Added Analyzable Interface

---
+++ src/main/java/com/dh/lab01/b/Analyzable.java
@@ -0,0 +1,10 @@
+package com.dh.lab01.b;
+
+/**
+ * @author Daniel Haddadeen (2588731)
+ */
+public interface Analyzable {
+    double getAverage();
+    GradedActivity getHighest();
+    GradedActivity getLowest();
+}
